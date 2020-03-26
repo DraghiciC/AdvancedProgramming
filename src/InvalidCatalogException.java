c52b3205cbc087f438dd9b161b30d77a4d534485
DraghiciC <draghici.constantin.danut@gmail.com>
2020-03-26 17:37:59 +0200

    Signed-off-by: DraghiciC <draghici.constantin.danut@gmail.com> Advanced programming lab6

--- src/InvalidCatalogException.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-public class InvalidCatalogException extends Exception {
- public InvalidCatalogException(Exception ex) {
- super("Can't load. Invalid catalog file.", ex);
- }
-}
-
